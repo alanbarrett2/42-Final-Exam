7d9727fa1b8b096ab758ef367a284755b28a21ce
Alan Roger Barrett II <41269411+alanbarrett2@users.noreply.github.com>
2019-01-21 16:55:34 -0800

    Create How to test atoi 2019.c

---
+++ 2-0-ft_atoi/How to test atoi 2019.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albarret <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/21 16:19:37 by albarret          #+#    #+#             */
+/*   Updated: 2019/01/21 16:48:51 by albarret         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int		ft_atoi(const char *str)
+{
+	int i;
+	int sign;
+	int n;
+
+	i = 0;
+	sign = 1;
+	while (((str[i] >= 9 && str[i] <= 13) || str[i] == 32) && str[i])
+		i++;
+	if (str[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else if (str[i] == '+')
+		i++;
+	while ((str[i] >= '0' && str[i] <= '9') && str[i])
+	{
+		n = n * 10 + (str[i] - '0');
+		i++;
+	}
+	return (n * sign);
+}
+
+int		main(void)
+{
+	printf("%d\n", ft_atoi("123456789"));
+	printf("%d\n", atoi("123456789"));
+	return (0);
+}
